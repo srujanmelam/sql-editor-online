4975de56ef5537784ccb23c3215a2c30107b0393
Chandra Sekhar <70321283+Parz-99@users.noreply.github.com>
2024-01-11 12:21:10 +0530

    Update sample.py - added aws Put,Get api's

--- sample.py
+++ sample.py
@@ -3,6 +3,9 @@ from flask import Flask,render_template,url_for,request
 import sqlite3
 import pandas as pd
 import re
+import boto3
+import os
+
 conn = sqlite3.connect('data/world.sqlite',check_same_thread=False)
 c = conn.cursor()
 
@@ -40,6 +43,30 @@ def remove_comments(sql):
 
 #init app
 app = Flask(__name__)
+
+AWS_ACCESS_KEY = 'your_access_key'
+AWS_SECRET_KEY = 'your_secret_key'
+AWS_REGION = 'your_aws_region'
+S3_BUCKET_NAME = 'your_s3_bucket_name'
+
+
+# Get the filepath of the currently executing Python script
+current_file_path = os.path.abspath(__file__)
+
+print(f"The current file path is: {current_file_path}")
+
+
+def deleteFileWithTimeOut(timeout,file_path):
+    try:
+        time.sleep(timeout)
+
+        # Delete the file
+        os.remove(file_path)
+        print(f"File deleted successfully.")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    return
+
 
 #routes
 @app.route('/')
@@ -83,6 +110,107 @@ def process_query():
         print("Errors are: ", errors)
         #,results = styled_html
     return render_template('index.html',results=styled_html,raw_query = raw_query,success_queries = success_queries,errors = errors)
+
+
+@app.route('/awsPutFile', methods=['POST'])
+def awsPutFile():
+    try:
+        # Get parameters from the request
+        # userId = request.args.get('luid')
+        uuid = request.args.get('attachment_uuid')
+        dbName = request.args.get('attachment_name')
+
+        # Initialize S3 client
+        s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY, region_name=AWS_REGION)
+
+        # Specify the file path on the server
+        local_file_path = current_file_path + '/data/'+dbName+'.sqlite'  # Change this to your local file path
+
+        try:
+            with open(local_file_path, 'x') as file:
+                # file.write("Hello, this is a new file!")
+                print(f"File created successfully at path: {local_file_path}")
+                        # Add a delay before deleting the file (e.g., 5 seconds)
+                delete_timeout = 4*60*60 #4hrs
+                print(f"Waiting for {delete_timeout} seconds before deleting the file...")
+                deleteFileWithTimeOut(delete_timeout,local_file_path)
+
+        except FileExistsError:
+            print(f"File already exists at path: {local_file_path}")
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+        # Specify S3 key (object name in S3)
+        s3_key = uuid  # You can adjust the key structure as needed
+
+        # Specify additional metadata
+        metadata = {
+            'ContentType': 'application/sqlite',
+            'ContentDisposition': 'attachment;filename='+dbName
+        }
+
+        # Upload file to S3 with metadata
+        s3.upload_file(local_file_path, S3_BUCKET_NAME, s3_key, ExtraArgs={'Metadata': metadata})
+
+        return "File uploaded successfully!"
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+
+
+@app.route('/awsGetFile', methods=['GET'])
+def awsGetFile():
+    try:
+
+                # Get parameters from the request
+        # userId = request.args.get('luid')
+        s3_key = request.args.get('attachment_uuid')
+        # dbName = request.args.get('attachment_name')
+
+        # Initialize S3 client
+        s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY, region_name=AWS_REGION)
+
+        # Specify the local file path to save the downloaded file
+        local_file_path = current_file_path + '/data/sample.sqlite'  # Change this to your desired local file path
+
+        # Download the file from S3 along with metadata
+        s3.download_file(S3_BUCKET_NAME, s3_key, local_file_path)
+
+        # Get metadata for the S3 object
+        metadata = s3.head_object(Bucket=S3_BUCKET_NAME, Key=s3_key)['Metadata']
+
+        contentType = metadata.ContentType;
+        contentDisposition = metadata.ContentDisposition;
+        contDispArr = contentDisposition.split("=");
+        contTypeArr = contentType.split("/");
+        new_file_path = current_file_path + '/data/' + contDispArr[1]+"."+contTypeArr[1];
+
+        try:
+            # Rename the file
+            os.rename(local_file_path, new_file_path)
+            delete_timeout = 5 #5 sec
+            print(f"Waiting for {delete_timeout} seconds before deleting the file...")
+            deleteFileWithTimeOut(delete_timeout,local_file_path)
+            delete_timeout = 4*60*60 #4hrs
+            deleteFileWithTimeOut(delete_timeout,new_file_path)
+
+            print(f"File renamed successfully from {current_file_path} to {new_file_path}")
+        except FileNotFoundError:
+            print(f"The file {current_file_path} does not exist.")
+        except FileExistsError:
+            print(f"The file {new_file_path} already exists.")
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+
+        # Return the file and metadata in the response
+        return send_file(local_file_path, as_attachment=True, download_name=f'{s3_key.split("/")[-1]}', attachment_filename=f'{s3_key.split("/")[-1]}', add_etags=False, cache_timeout=0, conditional=True, metadata=metadata)
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
 
 
 if __name__ == '__main__':
